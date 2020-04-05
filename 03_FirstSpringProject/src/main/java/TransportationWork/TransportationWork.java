3120421ebfc33a2b11060643a870ab1536223e2c
Choi jaehyun <emgjh92@naver.com>
2020-04-05 11:28:07 +0900

    2020-04-05
    2020-04-05

---
+++ 03_FirstSpringProject/src/main/java/TransportationWork/TransportationWork.java
@@ -0,0 +1,10 @@
+package TransportationWork;
+
+public class TransportationWork {
+
+	public void move() {
+		
+		System.out.println("도보로 이동합니다.");
+	}
+	
+}
---
+++ 03_FirstSpringProject/src/main/java/TransportationWork/mainClass.java
@@ -0,0 +1,16 @@
+package TransportationWork;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class mainClass {
+public mainClass() {
+	//TransportationWork twork = new TransportationWork();
+	//twork.move();
+	
+	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationPath.xml");
+	
+	ctx.getBean("tWalk, ")
+
+}
+
+}
